e0835d26ecfc7c3d5aff94dddb9aaaec91e469ec
Yann Soubeyrand <yann.soubeyrand@camptocamp.com>
2024-03-12 19:08:54 +0100

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import * as process from 'process'
 import * as fs from 'fs/promises'
 import * as core from '@actions/core'
-import { Octokit } from '@octokit/core'
+import * as github from '@actions/github'
 import { HttpClient } from '@actions/http-client'
 
 import Action from './action'
 
 await async function (): Promise<void> {
   try {
-    const octokit = new Octokit()
+    const githubToken = process.env['GITHUB_TOKEN']
+
+    if (githubToken === undefined) {
+      throw new Error('failed to get GITHUB_TOKEN environment variable')
+    }
+
+    const octokit = github.getOctokit(githubToken)
     const httpClient = new HttpClient()
 
     const module = JSON.parse((await fs.readFile('dagger.json').catch((error) => {
