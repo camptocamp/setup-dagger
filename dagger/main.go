f7b4a87bed06be68c627d8e891afe2194a4d7997
Yann Soubeyrand <yann.soubeyrand@camptocamp.com>
2024-03-15 18:03:49 +0100

    wip

--- dagger/main.go
+++ dagger/main.go
@@ -3,5 +3,5 @@ package main
 type SetupDagger struct{}
 
 func (m *SetupDagger) Test() *Container {
-	return dag.Container().From("alpine:latest").WithExec([]string{"echo", "Hello world!"})
+	return dag.Container().From("alpine:latest").WithExec([]string{"echo", "It works!"})
 }
